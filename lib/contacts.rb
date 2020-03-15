0dcf00df19baad344486e4654f2dd4762ae558ee
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-03-15 16:41:13 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,7 +1,6 @@
 contacts = {
   "Jon Snow" => {
- # :name => "Jon" this is the same shit
-    name: "Jon",
+  name: "Jon",
     email: "jon_snow@thewall.we", 
     favorite_ice_cream_flavors: ["chocolate", "vanilla", "mint chip"],
         knows: nil
@@ -15,10 +14,8 @@ contacts = {
  
 def list_1(hash)
   hash.each do |person, data|
-    #at this level, "person" is Jon Snow or Freddy and "data" is a hash of key/value pairs
-    #to iterate over the "data" hash, we can use the following line: 
- 
-    data.each do |attribute, value|
+   
+  data.each do |attribute, value|
       puts "#{attribute}: #{value}"
     end
   end
@@ -28,13 +25,11 @@ end
 
 def list_2(hash)
   hash.each do |person, data|
-    #at this level, "person" is Jon Snow or Freddy and "data" is a hash of key/value pairs
-    #to iterate over the "data" hash, we can use the following line: 
- 
+   
     data.each do |attribute, value|
       if attribute == :favorite_ice_cream_flavors
         value.each do |flavor|
-          # here, each index element in an ice cream flavor string
+          
           puts "#{flavor}"
         end 
       end
